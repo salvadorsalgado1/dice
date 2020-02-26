b697610ef4673694619b147f24deb042be724d90
salvadorsalgado1 <salvadorsalgado5694@gmail.com>
2020-02-25 19:57:52 -0800

    Updates

---
+++ precache-manifest.b06bee60757da8641e4ba4aef1326757.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08f4ca491cb8d7ef70888d28505672d8",
+    "url": "/dice/index.html"
+  },
+  {
+    "revision": "5118c10757e91ec2cbe3",
+    "url": "/dice/static/css/main.ba9beae4.chunk.css"
+  },
+  {
+    "revision": "c5b82a571fc001506e53",
+    "url": "/dice/static/js/2.8305a257.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/dice/static/js/2.8305a257.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5118c10757e91ec2cbe3",
+    "url": "/dice/static/js/main.487d8358.chunk.js"
+  },
+  {
+    "revision": "674efaa877bc48ed6ce3",
+    "url": "/dice/static/js/runtime-main.f09d01dc.js"
+  },
+  {
+    "revision": "e23497b3e5a103d168c5faa4ac1eb68b",
+    "url": "/dice/static/media/snow.e23497b3.jpg"
+  }
+]);
